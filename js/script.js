01f160c1a794f0c3e60d1b27ec7f046cbae0e8b3
Elijah Quesada <this_boy10@yahoo.com>
2019-06-13 18:10:19 -0700

    Added dynamic select option under t-shirt fieldset

--- js/script.js
+++ js/script.js
@@ -1,22 +1,76 @@
 $( document ).ready(function() {
 
 
-//name field
+//on page load
 $("#name").focus();
+$("input#other-title").hide();
+$("select#color").hide();
 
-//appends input field dynamically to the DOM.
-$("select#title").on("change", function (event) {
 
-  const inputFieldElement = $("<input type='text' id='other-title' placeholder='Your Job role'> ");
-
-  const basicInfoFieldSet = $(event.target).parent();
-
-  const selectOption = this;
-
-  if (selectOption.value === "other") {
-
-    basicInfoFieldSet.append(inputFieldElement);
+// takes 1 arg an object with 3 key/value pairs
+function showSelectedThemeColors(typeOfShirtsObject) {
+  if (typeOfShirtsObject.optionValue === 1) {
+     typeOfShirtsObject.jsPunsShirts.show();
+     typeOfShirtsObject.jsPunsShirts.eq(0).attr("selected", "selected");
+     typeOfShirtsObject.jsOnly.hide();
+  } else if (typeOfShirtsObject.optionValue === 2) {
+    typeOfShirtsObject.jsOnly.show();
+    typeOfShirtsObject.jsOnly.eq(0).attr("selected", "selected");
+    typeOfShirtsObject.jsPunsShirts.hide();
+  } else {
+     $("select#color").hide();
   }
+
+}
+
+
+// Basic Info fieldset; Job role selection
+$("select#title").on("change", function (event) {
+  const otherInputElement = $("input#other-title");
+  this.value == "other" ? otherInputElement.slideDown() : otherInputElement.slideUp();
 });
+
+
+// T-Shirt Info color selection.
+$("select#design").on("change", function() {
+  $("select#color").show();
+  $("select#color *").removeAttr("selected");
+
+ const typeOfShirts = {
+    jsPunsShirts: $("select#color option").slice(0, 3),
+    jsOnly: $("select#color option").slice(3),
+    optionValue: this.selectedIndex
+ }
+      showSelectedThemeColors(typeOfShirts);
+});
+
+
+//Register For Activities FieldSet
+let amount =  0;
+$("input[type='checkbox']").on("change",function (event) {
+const isCheckedIndex =  $("input[type='checkbox']").index(this);
+console.log(isCheckedIndex);
+switch(isCheckedIndex) {
+       case 1:
+         console.log("Hello");
+         break;
+       case 2:
+         console.log("hello from 2");
+         break;
+
+  }
+
+  // amount code
+  // const activityAmount = $(this)
+  // .parent()
+  // .text()
+  // .split(" ")
+  // .pop()
+  // .slice(1);
+  // amount += parseInt(activityAmount);
+
+});
+
+
 
 });
