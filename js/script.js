ce01178667754a7c0e4c1bcef52f64abdc7b1a7f
Elijah Quesada <this_boy10@yahoo.com>
2019-06-15 11:33:55 -0700

    Added checkbox functionality that allows the user to see what activites are avaiable depending on what time slot they already booked or checked. The from also allows the user to see the total with each selection

--- js/script.js
+++ js/script.js
@@ -1,76 +1,142 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+
+  //on page load
+  $("#name").focus();
+  $("input#other-title").hide();
+  $("select#color").hide();
 
 
-//on page load
-$("#name").focus();
-$("input#other-title").hide();
-$("select#color").hide();
-
-
-// takes 1 arg an object with 3 key/value pairs
-function showSelectedThemeColors(typeOfShirtsObject) {
-  if (typeOfShirtsObject.optionValue === 1) {
-     typeOfShirtsObject.jsPunsShirts.show();
-     typeOfShirtsObject.jsPunsShirts.eq(0).attr("selected", "selected");
-     typeOfShirtsObject.jsOnly.hide();
-  } else if (typeOfShirtsObject.optionValue === 2) {
-    typeOfShirtsObject.jsOnly.show();
-    typeOfShirtsObject.jsOnly.eq(0).attr("selected", "selected");
-    typeOfShirtsObject.jsPunsShirts.hide();
-  } else {
-     $("select#color").hide();
-  }
-
-}
-
-
-// Basic Info fieldset; Job role selection
-$("select#title").on("change", function (event) {
-  const otherInputElement = $("input#other-title");
-  this.value == "other" ? otherInputElement.slideDown() : otherInputElement.slideUp();
-});
-
-
-// T-Shirt Info color selection.
-$("select#design").on("change", function() {
-  $("select#color").show();
-  $("select#color *").removeAttr("selected");
-
- const typeOfShirts = {
-    jsPunsShirts: $("select#color option").slice(0, 3),
-    jsOnly: $("select#color option").slice(3),
-    optionValue: this.selectedIndex
- }
-      showSelectedThemeColors(typeOfShirts);
-});
-
-
-//Register For Activities FieldSet
-let amount =  0;
-$("input[type='checkbox']").on("change",function (event) {
-const isCheckedIndex =  $("input[type='checkbox']").index(this);
-console.log(isCheckedIndex);
-switch(isCheckedIndex) {
-       case 1:
-         console.log("Hello");
-         break;
-       case 2:
-         console.log("hello from 2");
-         break;
+  // takes 1 arg an object with 3 key/value pairs
+  function showSelectedThemeColors(typeOfShirtsObject) {
+    if (typeOfShirtsObject.optionValue === 1) {
+      typeOfShirtsObject.jsPunsShirts.show();
+      typeOfShirtsObject.jsPunsShirts.eq(0).attr("selected", "selected");
+      typeOfShirtsObject.jsOnly.hide();
+    } else if (typeOfShirtsObject.optionValue === 2) {
+      typeOfShirtsObject.jsOnly.show();
+      typeOfShirtsObject.jsOnly.eq(0).attr("selected", "selected");
+      typeOfShirtsObject.jsPunsShirts.hide();
+    } else {
+      $("select#color").hide();
+    }
 
   }
 
-  // amount code
-  // const activityAmount = $(this)
-  // .parent()
-  // .text()
-  // .split(" ")
-  // .pop()
-  // .slice(1);
-  // amount += parseInt(activityAmount);
+// takes 2 arg; disables other activty that times overlap. Also darkens color.
+  function blockOtherActivityTimeSlot(isCheckedIndex, index) {
 
-});
+    $("input[type='checkbox']")
+    .eq(isCheckedIndex + index)
+    .attr("disabled", true)
+    .parent()
+    .css("color", "darkgrey");
+
+  }
+
+// takes 2 arg; sets attr disabled to false and color to black.
+  function unblockOtherActivityTimeSlot(isCheckedIndex, index) {
+
+    $("input[type='checkbox']")
+    .eq(isCheckedIndex + index)
+    .attr("disabled", false)
+    .parent()
+    .css("color", "black");
+  }
+
+// takes this arg from checkbox on change listener; pulls text and parse into number type
+  function getCheckBoxActivityAmount(that) {
+
+    const activityAmount = $(that)
+    .parent()
+    .text()
+    .split(" ")
+    .pop()
+    .slice(1);
+    return parseInt(activityAmount);
+  }
+
+// takes a number parsed arg; and updates DOM with new number
+  function calculateAcitivtyTotalAndUpdate(activityAmount) {
+    let currentAmount = parseInt($("span#total-activities").text());
+    currentAmount += activityAmount;
+    $("span#total-activities").empty().text(currentAmount);
+
+  }
 
 
+  // Basic Info fieldset; Job role selection
+  $("select#title").on("change", function(event) {
+    const otherInputElement = $("input#other-title");
+    this.value == "other"
+      ? otherInputElement.slideDown()
+      : otherInputElement.slideUp();
+  });
 
+
+  // T-Shirt Info color selection.
+  $("select#design").on("change", function() {
+    $("select#color").show();
+    $("select#color *").removeAttr("selected");
+
+    const typeOfShirts = {
+      jsPunsShirts: $("select#color option").slice(0, 3),
+      jsOnly: $("select#color option").slice(3),
+      optionValue: this.selectedIndex
+    }
+
+    showSelectedThemeColors(typeOfShirts);
+
+  });
+
+
+  //Register For Activities FieldSet
+  $("input[type='checkbox']").on("change", function(event) {
+
+    const theIndexOfWhatCheckBoxWasChecked = $("input[type='checkbox']").index(this);
+
+    if ($(event.target).prop("checked")) {
+
+      const total = getCheckBoxActivityAmount(this);
+      calculateAcitivtyTotalAndUpdate(total);
+
+      switch (theIndexOfWhatCheckBoxWasChecked) {
+        case 1:
+          blockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, 2);
+          break;
+        case 2:
+          blockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, 2);
+          break;
+        case 3:
+          blockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, -2);
+          break;
+        case 4:
+          blockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, -2);
+          break;
+      }
+
+    } else {
+
+      const total = getCheckBoxActivityAmount(this);
+      calculateAcitivtyTotalAndUpdate(-total);
+
+      switch (theIndexOfWhatCheckBoxWasChecked) {
+        case 1:
+          unblockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, 2);
+          break;
+        case 2:
+          unblockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, 2);
+          break;
+        case 3:
+          unblockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, -2);
+          break;
+        case 4:
+          unblockOtherActivityTimeSlot(theIndexOfWhatCheckBoxWasChecked, -2);
+          break;
+      }
+
+    }
+
+  });
+
+  //end of jquery closure
 });
